13fcb550f1903c5525eb7a85f3f82b8b91318de1
田栗信昭 <taguri@gicon.jp>
2015-07-17 15:16:00 +0900

    chefレシピの追加 avahi_deamon

---
+++ chef-repo/site-cookbooks/avahi_install/metadata.rb
@@ -0,0 +1,7 @@
+name             'avahi_install'
+maintainer       'GIC Co.,Ltd.'
+maintainer_email 'taguri <taguri@gicon.jp>'
+license          'All rights reserved'
+description      'Installs/Configures avahi_install'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ chef-repo/site-cookbooks/avahi_install/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: avahi_install
+# Recipe:: default
+#
+# Copyright 2015, GIC Co.,Ltd.
+#
+
+package "avahi-daemon" do
+  action :install
+end
